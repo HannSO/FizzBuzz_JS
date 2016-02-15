510de1a8d56362ed4aac51ecd58c0715381f02b0
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-02-15 16:19:02 +0000

    completed fizz buzz numbers

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -9,4 +9,18 @@ describe('FizzBuzz', function() {
   it('returns "fizz" when passed 3', function() {
   	expect(fizzBuzz.play(3)).toEqual('fizz');
   });
+
+
+  it('returns "buzz" when passed 5', function() {
+    expect(fizzBuzz.play(5)).toEqual('buzz');
+  })
+
+
+  it('returns returns "fizzbuzz" when passed 15', function () {
+    expect(fizzBuzz.play(15)).toEqual('fizzbuzz');
+  })
+
+  it('returns 7 when passed 7', function () {
+    expect(fizzBuzz.play(7)).toEqual(7);
+  })
 });
